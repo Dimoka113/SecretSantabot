4072a314b74dbd75291f8c051d850c6619d30c39
Dimoka113 <133571582+Dimoka113@users.noreply.github.com>
2025-12-18 04:51:06 +0400

    Fix button call (back_profile_dat)

--- Handlers/Callback/open_profile.py
+++ Handlers/Callback/open_profile.py
@@ -17,39 +17,26 @@ def is_back_profile(data: types.CallbackQuery):
 async def open_profile_data(origin: Client, data: types.CallbackQuery):
     userdata = users.get_user_by_id(data.from_user.id)
     await data.edit_message_text(
-#             text="""
-# Ваш профиль:
-# ————————
-# Псевдоним: {name}
-# ————————
-# Возраст: {age}
-# ————————
-# О себе: 
-# {bio}
-# ————————
-# Ваши пожелания: 
-# {wishlist}
-# ————————
-# Ваши ссылки: 
-# {links}
-
-# Выберите то, что вы хотите изменить:
-# """
-            text = lang._text("formating_profile_status","profile")
-.format(
-    name=userdata["Name"] if userdata["Name"] else lang._text("data.null"), 
-    age=userdata["Age"] if userdata["Age"] else lang._text("data.null"), 
-    bio=userdata["Bio"] if userdata["Bio"] else lang._text("data.null"), 
-    wishlist=userdata["Wishlist"] if userdata["Wishlist"] else lang._text("data.null"),  
-    links=userdata["Soc_Nets"] if userdata["Soc_Nets"] else lang._text("data.null"), 
+    text=lang._text("formating_profile_status","profile")
+    .format(
+        name=userdata["Name"] if userdata["Name"] else lang._text("data.null"), 
+        age=userdata["Age"] if userdata["Age"] else lang._text("data.null"), 
+        bio=userdata["Bio"] if userdata["Bio"] else lang._text("data.null"), 
+        wishlist=userdata["Wishlist"] if userdata["Wishlist"] else lang._text("data.null"),  
+        links=userdata["Soc_Nets"] if userdata["Soc_Nets"] else lang._text("data.null"), 
     ),
-reply_markup=Keybords.keys_open_profile("start.main")
+    reply_markup=Keybords.keys_open_profile("start.main")
 )
 
 
 @bot.on_callback_query(lambda orig, data: is_back_profile(data))
 async def back_profile_data(origin: Client, data: types.CallbackQuery):
-    await data.edit_message_text(
-        text=lang._text("start_message"), 
-        reply_markup=Keybords.get_start_no_room()
-    )
+    if users.is_users_exist(data.from_user.id):
+        if len(users.get_user_rooms(data.from_user.id)) != 0:
+            text=lang._text("start_message"); reply_markup=Keybords.get_start_all()
+        else:
+            text=lang._text("start_message"); reply_markup=Keybords.get_start_no_room()
+    else:
+        text=lang._text("start_message_noprofile"); reply_markup=Keybords.create_profile()
+
+    await data.message.edit_text(text=text, reply_markup=reply_markup)
